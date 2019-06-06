a4d9f09c9b202cc0530edc0cc2bca4dbe7b4c90e
Mohit Yadav <mohit111198@gmail.com>
2019-06-06 16:34:44 +0530

    Rest API

---
+++ app.js
@@ -0,0 +1,105 @@
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
+
+mongoose.connect("mongodb://localhost/restful_app", {useNewUrlParser : true});
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.use(methodOverride());
+
+var blogSchema = new mongoose.Schema({
+	title : String,
+	image : String,
+	body : String,
+	created : {type:Date, default: Date.now}
+})
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get("/", function(req, res){
+	res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log("ERROR!");
+		}
+		else {
+			res.render("index", {blogs : blogs});
+		}
+	});
+});
+
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+app.post("/blogs", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body)
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");	
+		}
+		else {
+			res.redirect("/blogs");
+		}
+	});
+});
+
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else{
+			res.render("show", {blog : foundBlog});
+		}
+	})
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else {
+			res.render("edit", {blog:foundBlog});
+		}
+	});
+});
+
+app.put("/blogs/:id", function(req, res){
+		req.body.blog.body = req.sanitize(req.body.blog.body)
+		Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else {
+			res.redirect("/blogs/"+ req.params.id);
+		}
+	});
+
+});
+
+app.delete("/blogs/:id", function(req, res){
+	Blog.findOneAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		}
+		else{
+			res.redirect("/blogs");
+		}
+	})
+});
+
+
+app.listen(3000, 'localhost', function(){
+	console.log("Server has started on PORT 3000");
+});
